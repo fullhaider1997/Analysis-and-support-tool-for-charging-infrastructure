eed0088248c569a19a3839cf850547a66ad044ee
maikenapuada <maikenapuada@hotmail.ca>
2022-03-23 01:34:33 -0400

    working sched table. Note change directory

--- ENVI_SIM/routes/static/routes/routes_utilities.js
+++ ENVI_SIM/routes/static/routes/routes_utilities.js
@@ -14,45 +14,21 @@ function routeTable(path){
 
 
 function makeTable(data){
-    console.log("start table" + data);
-    
-  var col = [];
-  for (var i = 0; i < data.length; i++) {
-      for (var key in data[i]) {
-          if (col.indexOf(key) === -1) {
-              col.push(key);
-          }
-          console.log(key);
-      }
-  }
-  /* var table = document.createElement("table");
-  // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
+    var table = document.createElement("table");
+    for(var i in data){
+        var tr= table.insertRow(i)
+        for(var j in data[i])
+        {
+            var tabCell= tr.insertCell(j);
+            tabCell.innerHTML= data[i][j];
+           
+        }
+    }
+    // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
+    var divContainer = document.getElementById("dvCSV");
+    divContainer.innerHTML = "";
+    divContainer.appendChild(table);
 
-  var tr = table.insertRow(-1);                   // TABLE ROW.
 
-  for (var i = 0; i < col.length; i++) {
-      var th = document.createElement("th");      // TABLE HEADER.
-      th.innerHTML = col[i];
-      tr.appendChild(th);
-  }
-
-  // ADD JSON DATA TO THE TABLE AS ROWS.
-  for (var i = 0; i < data.length; i++) {
-
-      tr = table.insertRow(-1);
-
-      for (var j = 0; j < col.length; j++) {
-          var tabCell = tr.insertCell(-1);
-          tabCell.innerHTML = data[i][col[j]];
-      }
-  }
-
-  console.log("reached end of make table");
-
-  // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-  var divContainer = document.getElementById("dvCSV");
-  divContainer.innerHTML = "";
-  divContainer.appendChild(table);
- */
 
 }
